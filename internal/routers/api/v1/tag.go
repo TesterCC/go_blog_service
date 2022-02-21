5b42abf6664bfe11ab253336bec8339056974bc1
TesterCC <testerlyx@foxmail.com>
2022-02-21 18:02:24 +0800

    update - v1 tag

--- internal/routers/api/v1/tag.go
+++ internal/routers/api/v1/tag.go
@@ -1,5 +1,7 @@
 package v1
 
+import "github.com/gin-gonic/gin"
+
 type Tag struct {
 
 }
@@ -8,4 +10,8 @@ func NewTag() Tag{
 	return Tag{}
 }
 
-// P2.2.5处理程序 P81
+func (t Tag) Get(c *gin.Context) {}
+func (t Tag) List(c *gin.Context) {}
+func (t Tag) Create(c *gin.Context) {}
+func (t Tag) Update(c *gin.Context) {}
+func (t Tag) Delete(c *gin.Context) {}
