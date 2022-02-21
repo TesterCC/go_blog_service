4fb2e05b00f52bac2825bce79e4ff4a6742010f8
TesterCC <testerlyx@foxmail.com>
2022-02-21 22:34:33 +0800

    update - 2.2 project basic design

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -1,26 +1,33 @@
 package routers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	v1 "github.com/testercc/blog-service/internal/routers/api/v1"
+)
 
 func NewRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())   // 输出请求日志，并标准化日志的格式
 	r.Use(gin.Recovery()) // 异常捕获，针对每次请求处理进行Recovery处理，防止因为出现panic导致服务崩溃，同时将异常日志的格式标准化
 
+	// 编写好路由的 Handler 方法后，只需要将其注册到对应的路由规则上
+	article := v1.NewArticle()
+	tag := v1.NewTag()
+
 	apiv1 := r.Group("/api/v1")
 	{
-		apiv1.POST("/tags")
-		apiv1.DELETE("/tags/:id")
-		apiv1.PUT("/tags/:id")
-		apiv1.PATCH("/tags/:id/state")
-		apiv1.GET("/tags")
+		apiv1.POST("/tags", tag.Create)
+		apiv1.DELETE("/tags/:id", tag.Delete)
+		apiv1.PUT("/tags/:id", tag.Update)
+		apiv1.PATCH("/tags/:id/state", tag.Update)
+		apiv1.GET("/tags", tag.List)
 
-		apiv1.POST("/articles")
-		apiv1.DELETE("/articles/:id")
-		apiv1.PUT("/articles/:id")
-		apiv1.PATCH("/articles/:id/state")
-		apiv1.GET("/articles/:id")
-		apiv1.GET("/articles")
+		apiv1.POST("/articles",article.Create)
+		apiv1.DELETE("/articles/:id", article.Delete)
+		apiv1.PUT("/articles/:id", article.Update)
+		apiv1.PATCH("/articles/:id/state", article.Update)
+		apiv1.GET("/articles/:id", article.Get)
+		apiv1.GET("/articles", article.List)
 	}
 
 	return r
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"github.com/testercc/blog-service/internal/routers"
+	"net/http"
+	"time"
 )
 
+/*
 // curl http://127.0.0.1:8080/ping
 
 func main() {
@@ -14,3 +17,22 @@ func main() {
 	})
 	r.Run(":7777")  // default port 8080
 }
+*/
+
+// 改造为本项目的启动文件
+
+func main() {
+	router := routers.NewRouter()
+	// 通过自定义 http.Server，设置了监听的 TCP Endpoint、处理的程序、允许读取/写入的最大时间、请求头的最大字节数等基础参数
+	s := &http.Server {
+		Addr:           ":8080",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	// 开始监听
+	s.ListenAndServe()
+}
+
+// test: curl http://127.0.0.1:8080/api/v1/tags
