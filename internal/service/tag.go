4bc990c156b64a424d28da41779ca20cc6599fdb
TesterCC <testerlyx@foxmail.com>
2023-07-05 09:43:07 +0800

    update - CreateTagRequest struct, change Name limit

--- internal/service/tag.go
+++ internal/service/tag.go
@@ -25,7 +25,7 @@ type TagListRequest struct {
 }
 
 type CreateTagRequest struct {
-	Name      string `form:"name" binding:"required,min=2,max=100"`
+	Name      string `form:"name" binding:"required,min=1,max=100"`
 	CreatedBy string `form:"created_by" binding:"required,min=2,max=100"`
 	State     uint8  `form:"state,default=1" binding:"oneof=0 1"`
 }
